da0d41de0af47fb2e9a988ba746a87ce79209571
Steve Grunwell <stevegrunwell@gmail.com>
2016-12-21 23:24:19 -0500

    Add an easter egg to the console, linking to the GitHub repo for this project

--- wp-content/themes/growella-engineering/assets/js/scripts.js
+++ wp-content/themes/growella-engineering/assets/js/scripts.js
@@ -4,4 +4,7 @@
 	// Initialize HighlightJS.
 	hljs.initHighlightingOnLoad();
 
+	// Tease this site's GitHub repo in the console.
+	console.info( 'Psst...if you\'re curious, this site\'s source is available on GitHub: https://github.com/growella/engineering-blog' );
+
 } () );
